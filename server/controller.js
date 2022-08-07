2cf2fb3b0329093bc599c066eb574db79a391b64
Kanwal Mehmud <mehmudkanwal@gmail.com>
2022-08-07 00:44:27 -0400

    fixed server bug

--- server/controller.js
+++ server/controller.js
@@ -2,7 +2,7 @@ const hotels = require('./db.json')
 let globalId = 4
 
 module.exports = {
-    gethotels: (req, res) => res.status(200).send(hotels),
+    getHotels: (req, res) => res.status(200).send(hotels),
     deleteHotel: (req, res) => {
         let index = hotels.findIndex(elem => elem.id === +req.params.id)
         hotels.splice(index, 1)
