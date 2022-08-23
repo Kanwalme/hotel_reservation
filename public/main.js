e407564d27cd4c5c0421e122534e78df53799278
Kanwalme <mehmudkanwal@gmail.com>
2022-08-23 13:34:31 -0500

    deleted all notes

--- public/main.js
+++ public/main.js
@@ -1,13 +1,9 @@
-// Front end renders the view--what the user see on the screen. when the user wants to get data or to change something, the front end will communicate with the server by making Http requests(get/post/put/delete) using JSON data(thats how you talk to express (backend)). Server side, using express will call the database to get the data or modify the data. Database will send data back to Express, will format the data, and will send it back to the front end.
 
-//creates and appends hotel data used to display on the user-end.
 function populateHotels(hotel) {
   const section = document.querySelector('section')
-  //this while loop is used to display just one hotel selection at a time on the main page and to remove others from the section section if any more are selected.
   while (section.lastElementChild) {
     section.removeChild(section.lastElementChild)
   }
-// from lines 10-16, I am creating html elements used to store hotel information.
   const hotelImg = document.createElement('img')
   const selectedHotel = document.createElement('article')
   const hotelId = document.createElement('p')
@@ -16,7 +12,6 @@ function populateHotels(hotel) {
   const myBreakLine = document.createElement('br')
   const reserveButton = document.createElement('button')
   
-  //accessing information from the db.json file and adding the reserve now button to the appropriate elements we created.
   hotelImg.src = hotel.imageURL
   hotelLocation.textContent = `Location: ${hotel.location}`
   price.textContent = `Price per Night: $${hotel.pricePernight}`
@@ -24,7 +19,6 @@ function populateHotels(hotel) {
   reserveButton.classList.add('ReserveNowButton')
   reserveButton.textContent = 'Reserve Now'
 
-  //appending the hotel items to each hotel in the section section
   selectedHotel.appendChild(hotelImg)
   selectedHotel.appendChild(hotelId)
   selectedHotel.appendChild(hotelLocation)
